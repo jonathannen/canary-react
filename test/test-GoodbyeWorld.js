fbacb5b2fb5af4a25298e59ce6f5f45a5b4af179
Jon Williams <jon@jonathannen.com>
2017-01-04 18:28:49 -0600

    Tests passing

--- test/test-GoodbyeWorld.js
+++ test/test-GoodbyeWorld.js
@@ -1,18 +1,17 @@
 // (C) Copyright Jon Williams 2015 - See LICENSE file
 //
 
-import { expect, React, TestUtils } from './shared'
+import React from 'react';
+import { expect } from 'chai';
+import { shallow, mount, render } from 'enzyme';
+
 import GoodbyeWorldComponent from '../views/GoodbyeWorld';
-var GoodbyeWorld = TestUtils.wrapReactRouterContext(GoodbyeWorldComponent);
 
 describe('GoodbyeWorld', function() {
 
   it('be polite to those leaving', function() {
-    var component = TestUtils.renderIntoDocument(<GoodbyeWorld/>);
-    var h1 = TestUtils.findRenderedDOMComponentWithTag(component, 'h1')
-    expect(h1.getDOMNode().textContent).to.contain('Goodbye');
-    // If you want to see the rendered output
-    // console.log(component.getDOMNode().innerHTML);
+    var h1 = shallow(<GoodbyeWorldComponent />).find('h1').text();
+    expect(h1).to.contains('Goodbye');
   });
 
 });
